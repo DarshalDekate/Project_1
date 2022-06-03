4a98ac4cb7fbc45c5329088ea3c5ee6fb0739445
DarshalDekate <106420196+DarshalDekate@users.noreply.github.com>
2022-06-03 23:29:33 +0530

    Update problem2.js

--- problem2.js
+++ problem2.js
@@ -1,17 +1,10 @@
-function  problem2(inventory) {
+function  problem2(inventory) 
+    {
 
-    //document.write(inventory.length);
+    var last=inventory.length-1;{
     
-    var last=inventory.length-1;
-    
-         // console.log("ID :-" + inventory[last].id +" ");  
-          //console.log("Car Make :-" + inventory[last].car_make +" "); 
-         // console.log("Car Model :-" + inventory[last].car_model +" "); 
-          //console.log("Car Year :-" + inventory[last].car_year +" "); 
           console.log("Last car is a" + inventory[last].id +"  "+ inventory[last].car_model+" ")
     
-    
-          
-    
     }
+}
     module.exports = problem2;
