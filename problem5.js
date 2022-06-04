49740cf42d8fc4cd57e31b52ad964dc8fd085e30
DarshalDekate <106420196+DarshalDekate@users.noreply.github.com>
2022-06-04 16:55:04 +0530

    Update problem5.js

--- problem5.js
+++ problem5.js
@@ -2,11 +2,11 @@ function  problem5(inventory) {
     const car = [];
     for (i = 0; i < inventory.length; i++) 
     {
-          if(inventory[i].car_year>=2000)
+          if(inventory[i].car_year<2000)
           {
            car.push(inventory[i]);  
           }
     }
-    return car;
+    console.log(car.length);
     }
     module.exports = problem5;
