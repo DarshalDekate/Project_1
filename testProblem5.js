993cfa2c21f5c7eb2d9d4ac7384ee852346c8b9d
DarshalDekate <106420196+DarshalDekate@users.noreply.github.com>
2022-06-04 16:55:42 +0530

    Update testProblem5.js

--- testProblem5.js
+++ testProblem5.js
@@ -1,7 +1,3 @@
 var problem5 = require('./problem5.js');
 var inventory = require('./inventary.js');
-var car =problem5(inventory);
-for (i = 0; i < car.length; i++) 
-{
-	console.log(car[i].car_year +" ");
-}
+problem5(inventory);
